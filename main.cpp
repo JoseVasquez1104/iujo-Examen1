fc59adbe3e5b53aa407c9ed96ad5c7dc91c2bf1c
Jose Antonio <joseantoniovf.cmb@gmail.com>
2024-06-30 15:04:14 -0400

    Segundo Commit Preparacion del Enunciado

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* Integrantes: José Vásquez, Luis Espinoza, Adrian Aguirre, Jesus Arrieche */
+
+/*
+Realize un programa que calculé el total de compras de un supermercado 
+los datos de entradas son: razón social cliente,rif/cédula.
+Calcular la cantidad de compras el mayor y detal
+Calcular el monto a pagar por cada cliente 
+Los productos son:
+Refresco 10$ detal al mayor 5$ al mayor es a partir de 6 unidades, 
+malta 12$ detal mayor 7$ al mayor es a partir de 6 unidades
+*/
+
+int main(int argc, char** argv) {
+	return 0;
+}
